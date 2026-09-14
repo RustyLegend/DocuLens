0fe20eea9ebc495961518982050865974bfa7610
Rahul Gunturu <rahulgunturu123@gmail.com>
2026-09-14 19:34:48 +0530

    created the first iteration of my RAG using langchain

--- RAG_using_langchain.py
+++ RAG_using_langchain.py
@@ -1,13 +1,54 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv
+import os
 
-loader = PyPDFLoader('Introduction to Machine Learning.pdf')
+#Load embedding model
+embedding_model = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
 
-documents = loader.load()
-splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
-RecursiveCharacterTextSplitter()
-chunks = splitter.split_documents(documents)
+#Load stored vector DB
+vectorbase = Chroma(
+    persist_directory='./chroma_db',
+    embedding_function=embedding_model
+)
 
-print('No.of chunks: ', len(chunks))
-print('Chunk data:\n', chunks[0].page_content)
-print('Chunk metadata:\n', chunks[0].metadata)
+#LLM model initialization
+load_dotenv()
+llm = GoogleGenerativeAI(
+    model = 'gemini-3.6-flash',
+)
+
+#Retrieve top 5 results for a query
+retriever = vectorbase.as_retriever(search_kwargs = {'k' : 5})
+query = input('Enter query: ')
+results = retriever.invoke(query)
+
+#Convert the top 5 results into context and build the prompt
+prompt = ChatPromptTemplate.from_template(
+    """
+Use only the information provided in the retrieved passages.
+Answer the question clearly and concisely.
+Cite the page or page range where the information came from.
+
+If the passages do not contain enough information to answer the
+question, say that the answer cannot be determined from the
+provided passages.
+
+Retrieved passages:
+{context}
+
+Question:
+{question}
+    """
+)
+context = '\n\n'.join(x.page_content for x in results)
+txt_prompt = prompt.invoke({
+    'context' : context,
+    'question' : query
+})
+
+#Generate response
+response = llm.invoke(txt_prompt)
+print(response)
