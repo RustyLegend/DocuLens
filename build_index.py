cb442cad0707c0d7ad343b3f7a5ed9cc1ef6d73f
Rahul Gunturu <rahulgunturu123@gmail.com>
2026-09-17 15:31:39 +0530

    converted the process into functions for better generalization

--- build_index.py
+++ build_index.py
@@ -4,19 +4,32 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 
 #Load document
-loader = PyPDFLoader('Introduction to Machine Learning.pdf')
-documents = loader.load()
+def load_document(path):
+    loader = PyPDFLoader(path)
+    documents = loader.load()
+    return documents
 
 #Create chunks of imported document
-splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
-chunks = splitter.split_documents(documents)
+def split_documents(documents):
+    splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+    chunks = splitter.split_documents(documents)
+    return chunks
 
 #Embed created chunks and store them in chroma DB
-embedding_model = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
-vectorbase = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding_model,
-    persist_directory='./chroma_db'
-)
+def create_vector_db(chunks, persist_dir):
+    embedding_model = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+    vectorbase = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_dir
+    )
+    return vectorbase
 
-print('No.of chunks stored in DB: ', vectorbase._collection.count())
+documents = load_document('Introduction to Machine Learning.pdf')
+print('No.of pages: ', len(documents))
+
+chunks = split_documents(documents)
+print('No.of chunks: ', len(chunks))
+
+vectorbase = create_vector_db(chunks, './chroma_db')
+print('No.of chunks added to vector DB: ', vectorbase._collection.count())
