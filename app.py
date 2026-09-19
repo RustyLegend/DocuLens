1fa6850dc3b20eeaf4aa4ab019ce3384b1d1ac8b
Rahul Gunturu <rahulgunturu123@gmail.com>
2026-09-19 20:28:42 +0530

    removed the retrieval and generation code

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 import tempfile
 from build_index import process_document
-from langchain_google_genai import GoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
-
-def create_context(results):
-    return '\n\n'.join(f"Page: {x.metadata['page']}\n{x.page_content}" for x in results)
+from rag import answer_rag
 
 #Get the uploaded file as temp file from streamlit
 def save_uploaded_file(uploaded_file):
@@ -36,38 +31,8 @@ if uploaded_file is not None:
         submitted = st.form_submit_button("Ask Question")
     
     if submitted and query:
-        retriever = vector_base.as_retriever(search_kwargs = {'k' : 5})
-        results = retriever.invoke(query)
-        context = create_context(results)
 
-        load_dotenv()
-        llm = GoogleGenerativeAI(
-            model = 'gemini-3.6-flash'
-        )
-
-        prompt = ChatPromptTemplate.from_template(
-        """
-        Use only the information provided in the retrieved passages.
-        Answer the question clearly and concisely.
-
-        If the passages do not contain enough information to answer the
-        question, say that the answer cannot be determined from the
-        provided passages.
-
-        Retrieved passages:
-        {context}
-
-        Question:
-        {question}
-            """
-        )
-
-        message = prompt.invoke({
-            'context' : context,
-            'question' : query
-        })
-
-        response = llm.invoke(message)
+        response, results = answer_rag(vector_base, query)
 
         st.write('### Answer')
         st.write(response)
