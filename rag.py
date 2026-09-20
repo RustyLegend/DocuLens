cc17ddd5c7f77e8f777c81cfdf94c927df391f7f
Rahul Gunturu <rahulgunturu123@gmail.com>
2026-09-20 17:53:22 +0530

    introduced query rewriting based on chat history

--- rag.py
+++ rag.py
@@ -5,6 +5,38 @@ from dotenv import load_dotenv
 def create_context(results):
     return '\n\n'.join(f"Page: {x.metadata['page']}\n{x.page_content}" for x in results)
 
+def rewrite_query(query, chat_history):
+    history = '\n\n'.join(f'User: {message['question']}\nAI: {message['answer']}' for message in chat_history)
+
+    rewrite_prompt = ChatPromptTemplate.from_template(
+            """
+    Given the conversation history and the user's latest question,
+    rewrite the latest question into a standalone question that can
+    be understood without the conversation history.
+
+    If the latest question is already standalone, return it unchanged.
+
+    Do not answer the question.
+
+    Conversation history:
+    {history}
+
+    Latest question:
+    {question}
+
+    Standalone question:
+    """
+        )
+
+    prompt = rewrite_prompt.invoke({
+        'history' : history,
+        'question' : query
+    })
+
+    response = llm.invoke(prompt)
+
+    return response
+
 prompt = ChatPromptTemplate.from_template(
         """
 Use only the information provided in the retrieved passages.
@@ -27,7 +59,7 @@ llm = GoogleGenerativeAI(
     model = 'gemini-3.6-flash'
 )
 
-def answer_rag(vector_base, query):
+def answer_rag(vector_base, query, chat_history):
     retriever = vector_base.as_retriever(
         search_type = 'mmr',
         search_kwargs = {
@@ -35,12 +67,15 @@ def answer_rag(vector_base, query):
             'fetch_k' : 20
         }
     )
-    results = retriever.invoke(query)
+
+    rewritten_query = rewrite_query(query, chat_history)
+
+    results = retriever.invoke(rewritten_query)
     context = create_context(results)
     
     message = prompt.invoke({
         'context' : context,
-        'question' : query
+        'question' : rewritten_query
     })
 
     response = llm.invoke(message)
